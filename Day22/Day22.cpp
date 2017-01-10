b31705ac8dc98fb0ba601e3e7d15ed9e4f3cd459
Tom Bulsink <nospam@sorrowed.nl>
2017-01-11 00:16:41 +0100

    - WIP: Add skeleton & inout for day 22

---
+++ Day22/Day22.cpp
@@ -0,0 +1,23 @@
+/*
+ * Day22.cpp
+ *
+ *  Created on: Jan 11, 2017
+ *      Author: tom
+ */
+#include "Day22.h"
+
+int Day22_Test( void )
+{
+	return -1;
+}
+int Day22_Part1( int argc, char* argv[] )
+{
+	return -1;
+}
+
+int Day22_Part2( int argc, char* argv[] )
+{
+	return -1;
+}
+
+
---
+++ Day22/Day22.h
@@ -0,0 +1,16 @@
+/*
+ * Day22.h
+ *
+ *  Created on: Jan 11, 2017
+ *      Author: tom
+ */
+
+#ifndef DAY22_DAY22_H_
+#define DAY22_DAY22_H_
+
+int Day22_Test( void );
+int Day22_Part1( int argc, char* argv[] );
+int Day22_Part2( int argc, char* argv[] );
+
+
+#endif /* DAY22_DAY22_H_ */
